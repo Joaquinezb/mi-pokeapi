1998ca01fd15bc9fb229e8895235345307608701
joaquinezb <kakintopjezb@gmail.com>
2025-07-01 17:09:40 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import PokemonFetcher from './PokemonFetcher';
+
+function App() {
+  return (
+    <>
+      <h1>conoce a tus pokemones</h1>
+       <PokemonFetcher />
+    </>
+  );
+} 
+
+export default App;
